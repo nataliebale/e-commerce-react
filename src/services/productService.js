39febdddf2ac7db2017a9738dc2772447e5e4aa5
nataliabalakhashvili <natalia.balakhashvili@geolab.edu.ge>
2023-11-23 20:02:28 +0400

    Update productService.js

--- src/services/productService.js
+++ src/services/productService.js
@@ -3,3 +3,7 @@ import axios from "axios";
 export const getProducts = () => {
   return axios.get('https://fakestoreapi.com/products');
 }
+
+export const getProductById = (id) => {
+  return axios.get(`https://fakestoreapi.com/products/${id}`);
+}
