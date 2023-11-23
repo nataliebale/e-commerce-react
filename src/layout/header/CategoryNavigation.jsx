009e91a02f1c6056830b609560a7d3c7bf53fe86
nataliabalakhashvili <natalia.balakhashvili@geolab.edu.ge>
2023-11-23 20:24:06 +0400

    Update CategoryNavigation.jsx

--- src/layout/header/CategoryNavigation.jsx
+++ src/layout/header/CategoryNavigation.jsx
@@ -1,30 +1,29 @@
 import {Link} from "react-router-dom";
+import {useEffect, useState} from "react";
+import {getCategories} from "../../services/categoryService.js";
 
 function CategoryNavigation() {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    getCategories().then(res => {
+      console.log(res.data);
+      setCategories(res.data)
+    })
+  }, []);
 
   return (
     <nav>
       <ul className='flex'>
-        <li className='pb-4 pr-3'>
-          <Link to={'/category'} className='uppercase'>
-            category
-          </Link>
-        </li>
-        <li className='pb-4 pr-3'>
-          <Link to={'/category'} className='uppercase'>
-            category
-          </Link>
-        </li>
-        <li className='pb-4 pr-3'>
-          <Link to={'/category'} className='uppercase'>
-            category
-          </Link>
-        </li>
-        <li className='pb-4 pr-3'>
-          <Link to={'/category'} className='uppercase'>
-            category
-          </Link>
-        </li>
+        { categories.map((category, index) => {
+          return (
+            <li className='pb-4 pr-3' key={index}>
+              <Link to={`/products?category=${category}`} className='uppercase'>
+                { category }
+              </Link>
+            </li>
+          );
+        })}
       </ul>
     </nav>
   )
