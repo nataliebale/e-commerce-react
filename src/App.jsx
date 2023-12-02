3703512e20007e483b379def299c2a877b42cb8d
nataliabalakhashvili <natalia.balakhashvili@geolab.edu.ge>
2023-12-02 23:02:18 +0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,19 +9,21 @@ import Home from "./pages/Home.jsx";
 import Header from "./layout/header/Header.jsx";
 import Products from "./pages/Products.jsx";
 import Product from "./pages/Product.jsx";
+import Footer from "./layout/footer/Footer.jsx";
 
 function App() {
   return (
     <Router>
       <>
         <Header></Header>
-        <main className='container m-auto flex justify-between'>
+        <main>
           <Routes>
             <Route path='/' element={<Home/>}></Route>
             <Route path='/products' element={<Products/>}></Route>
             <Route path='/products/:id' element={<Product/>}></Route>
           </Routes>
         </main>
+        <Footer></Footer>
       </>
     </Router>
   )
