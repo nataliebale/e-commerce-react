4264d132f2a3f701baed30cebc3229d87e0f0bb1
nataliabalakhashvili <natalia.balakhashvili@geolab.edu.ge>
2023-12-02 23:03:19 +0400

    Update Logo.jsx

--- src/layout/header/Logo.jsx
+++ src/layout/header/Logo.jsx
@@ -3,7 +3,10 @@ import { Link } from "react-router-dom";
 function Logo() {
   return (
     <div className='font-bold pt-6 pb-4 text-4xl'>
-      <Link to={'/'} className='text-secondary'>E-Store</Link>
+      <Link to={'/'}>
+        <span className="text-secondary">E</span>
+        -Store
+      </Link>
     </div>
   )
 }
