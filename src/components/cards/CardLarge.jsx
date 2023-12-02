e1a0a4c7d9f7af9a1b43888d94c2300442297632
nataliabalakhashvili <natalia.balakhashvili@geolab.edu.ge>
2023-12-02 23:02:50 +0400

    Update CardLarge.jsx

--- src/components/cards/CardLarge.jsx
+++ src/components/cards/CardLarge.jsx
@@ -5,22 +5,28 @@ function CardLarge({id, image, title, category, price}) {
   return (
     <Link
       to={`/products/${id}`}
-      className='block mt-4 border rounded-[5px] overflow-hidden'
+      className='block bg-white mt-4 border rounded-[5px] overflow-hidden'
     >
       <div className='p-4'>
         <div
           className='pt-[80%] bg-contain bg-no-repeat bg-center'
           style={{backgroundImage: `url(${ image })`}}
         ></div>
-        <div className='text-[18px] pt-4 bold'>
+        <div className='text-[18px] pt-4 bold overflow-hidden h-[4.4rem] line-clamp-2 flex truncate whitespace-normal'>
           { title }
         </div>
         <div className='text-[14px] py-1 text-grey'>
           { category }
         </div>
         <p>
-          { price }
+          { price } $
         </p>
+        <button className="w-full text-center p-2 mt-3 text-[12px] rounded-[5px] font-bold uppercase bg-grey-light">
+          Add to cart
+        </button>
+        {/*<button className="w-full text-center p-2 mt-3 text-[12px] rounded-[5px] text-white background-gradient">*/}
+        {/*  Add to cart*/}
+        {/*</button>*/}
       </div>
     </Link>
   )
