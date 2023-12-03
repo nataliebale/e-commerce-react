3e839b0029358ad7d2c8885f5505fca776214bba
nataliabalakhashvili <natalia.balakhashvili@geolab.edu.ge>
2023-12-04 00:49:52 +0400

    Update Footer.jsx

--- src/layout/footer/Footer.jsx
+++ src/layout/footer/Footer.jsx
@@ -1,7 +1,10 @@
 function Footer(){
   return (
     <footer>
-      <div className='footer-copyright'>
+      <div className='bg-dark-blue footer-copyright text-white text-center py-[30px]'>
+        footer text
+      </div>
+      <div className='bg-dark-blue border-t-dark-grey border-t footer-copyright text-white text-center py-[20px]'>
         © Copyright 2023
       </div>
     </footer>
