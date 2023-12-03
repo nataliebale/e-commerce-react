634d8b19520147752f89faff8ce265bc2534555e
nataliabalakhashvili <natalia.balakhashvili@geolab.edu.ge>
2023-12-03 23:59:14 +0400

    Update Subscription.jsx

--- src/components/subscription/Subscription.jsx
+++ src/components/subscription/Subscription.jsx
@@ -8,7 +8,7 @@ function Subscription() {
       <div
         className="container m-auto py-[80px] px-[80px] rounded-[5px] background-gradient"
       >
-        <div className='bg-white h-[50px] flex rounded-[50px] my-6 border border-gray-200 overflow-hidden'>
+        <div className='bg-white w-[60%] m-auto h-[50px] flex rounded-[50px] my-6 border border-gray-200 overflow-hidden'>
           <input type="text" placeholder='Search...' className='rounded-[50px] px-6 w-[calc(100%-36px)] block'/>
           <div className='w-[40px] h-[40px] py-[12px]'>
             <SearchIcon></SearchIcon>
