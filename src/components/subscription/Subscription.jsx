8c83a8fe448fa783d23436b4f983f4f5b57d0327
nataliabalakhashvili <natalia.balakhashvili@geolab.edu.ge>
2023-12-04 01:08:04 +0400

    Update Subscription.jsx

--- src/components/subscription/Subscription.jsx
+++ src/components/subscription/Subscription.jsx
@@ -8,12 +8,18 @@ function Subscription() {
       <div
         className="container m-auto py-[80px] px-[80px] rounded-[5px] background-gradient"
       >
+        <p className="text-center text-white text-[30px]">
+          Subscribe Now!
+        </p>
         <div className='bg-white w-[60%] m-auto h-[50px] flex rounded-[50px] my-6 border border-gray-200 overflow-hidden'>
-          <input type="text" placeholder='Search...' className='rounded-[50px] px-6 w-[calc(100%-36px)] block'/>
+          <input type="text" placeholder='Drop your e-mail' className='rounded-[50px] px-6 w-[calc(100%-36px)] block'/>
           <div className='w-[40px] h-[40px] py-[12px]'>
             <SearchIcon></SearchIcon>
           </div>
         </div>
+        <p className="text-center text-white text-[16px]">
+          Be the first to know: sign up for updates & deals!
+        </p>
       </div>
     </div>
   )
